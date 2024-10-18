7c1df908765aa3c1f12674abee1b32c4c6daeca4
Carlos Ramos <carlos.dg147@gmail.com>
2024-10-18 18:34:40 -0400

    Inicio del proyecto.

---
+++ src/main/java/com/infsis/socialpagebackend/SocialpagebackendApplication.java
@@ -0,0 +1,14 @@
+package com.infsis.socialpagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SocialpagebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialpagebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infsis/socialpagebackend/SocialpagebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infsis.socialpagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialpagebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
