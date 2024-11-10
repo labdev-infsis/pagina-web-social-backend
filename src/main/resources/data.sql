8b297569e36713e265b2c616a294e9104b1caef1
RodrigoGutierrez <rodrigo.gutierrez.acc@gmail.com>
2024-11-10 13:14:09 -0400

    Fixed creating multiple posts and added data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,21 @@
+
+insert into role(id_role, name)
+    values(1, 'ADMIN'),
+    (2, 'STUDENT');
+
+insert into comment_config (id, uuid, configuration)
+    values(1, '875d7d7f-7a1c-4b77-ab63-77a9f76759d0', 'Todos pueden comentar'),
+    (2, '587d7d7f-5g3n-4b77-cf98-77a9h46759d0', 'Nadie puede comentar'),
+    (3, '492d7d7f-9f4s-8g45-hy34-77a9h46759d0', 'Comentarios con moderador');
+
 insert into institution (id, uuid, name, description, location, category, email, phone, url, logo_url, background_url)
-    values(1000, '7887b1dc-f947-43c1-be80-1f8e939042e3',
-            'Facultad de Ciencias y Tecnología', 
-            'La Facultad de Ciencias y Tecnología, contribuye al desarrollo del País en el campo de la formación de nuevos profesionales.', 
+    values(1, '77f803b4-f63b-4c4a-be05-eae84cef0c0c',
+            'Ingeniería Eléctrica',
+            'Servicios, actividades y noticias de la carrera de Ingeniería Eléctrica de la FCyT(UMSS)',
             'Campus Central UMSS (frente al Parque la Torre) Cochabamba, Bolivia', 
             'Universidad', 
-            'informacion@fcyt.umss.edu.bo', 
+            'electrica@fcyt.umss.edu.bo',
             '+591 4 4543232',
-            'fcyt.umss.edu.bo',
-            'https://i.postimg.cc/pr9tS8Yr/FCYT-Logo.png',
-            'https://i.postimg.cc/mkW0mqDX/Background-FCYT.jpg');
+            'electrica.fcyt.umss.edu.bo',
+            '/images/77f803b4o4534d',
+            '/images/eae84cef0c0cd3');
