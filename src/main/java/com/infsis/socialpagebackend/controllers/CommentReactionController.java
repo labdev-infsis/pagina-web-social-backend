61b857b98e7160b5f04780ec8103614592bb910e
Freddy Jiménez <fjimenezvc@gmail.com>
2024-12-04 10:21:40 -0400

    FE-15 Reaccionar a comentario

---
+++ src/main/java/com/infsis/socialpagebackend/controllers/CommentReactionController.java
@@ -0,0 +1,37 @@
+package com.infsis.socialpagebackend.controllers;
+
+import com.infsis.socialpagebackend.dtos.CommentReactionDTO;
+import com.infsis.socialpagebackend.services.CommentReactionService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+@Validated
+public class CommentReactionController {
+
+    @Autowired
+    private CommentReactionService commentReactionService;
+
+    @GetMapping("/comment/{commentUuid}/reactions/{reactionUuid}")
+    public CommentReactionDTO get(@PathVariable String commentUuid, @PathVariable String reactionUuid) {
+        return commentReactionService.getCommentReaction(commentUuid, reactionUuid);
+    }
+
+    @GetMapping("/comment/{commentUuid}/reactions")
+    public List<CommentReactionDTO> getAll(@PathVariable String commentUuid) {
+        return commentReactionService.getAllCommentReaction(commentUuid);
+    }
+
+    @PostMapping("/comment/{commentUuid}/reactions")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentReactionDTO create(@PathVariable String commentUuid, @Valid @RequestBody CommentReactionDTO commentReactionDTO) {
+        return commentReactionService.saveReaction(commentUuid, commentReactionDTO);
+    }
+
+}
---
+++ src/main/java/com/infsis/socialpagebackend/dtos/CommentReactionDTO.java
@@ -0,0 +1,30 @@
+package com.infsis.socialpagebackend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class CommentReactionDTO {
+
+    private String uuid;
+
+    @Size(min = 36, max = 40)
+    private String userId;
+
+    @Size(min = 36, max = 40)
+    private String commentId;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private Date reactionDate;
+
+    @NotBlank
+    @Size(min = 3, max = 36)
+    private String emojiTypeId;
+
+}
