fe6a77cc129a81c61ed014ebd542c7e6d1a316c3
ArgenesDev <argenes77@gmail.com>
2025-02-21 04:37:24 -0400

    ✨ [FE] Comments Interactions: Reactions and Replies Functionality

--- src/main/java/com/infsis/socialpagebackend/replies/dto/ReplyDTO.java
+++ src/main/java/com/infsis/socialpagebackend/replies/dto/ReplyDTO.java
@@ -3,12 +3,13 @@ package com.infsis.socialpagebackend.replies.dto;
 import com.infsis.socialpagebackend.posts.dtos.ReactionCounterDTO;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 public class ReplyDTO {
     private String uuid;
-    private Date createdDate;
+    private LocalDateTime createdDate;
     private String content;
     private String name;
     private String lastName;
