0b4dafd7af5013eb970a26a6a63bffdb19b21954
FabDevX <201809660@est.umss.edu>
2025-04-28 11:08:07 +0100

    already deploy dric

--- src/main/java/com/infsis/socialpagebackend/SocialpagebackendApplication.java
+++ src/main/java/com/infsis/socialpagebackend/SocialpagebackendApplication.java
@@ -2,12 +2,18 @@ package com.infsis.socialpagebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SocialpagebackendApplication {
+public class SocialpagebackendApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SocialpagebackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialpagebackendApplication.class, args);
 	}
-
 }
