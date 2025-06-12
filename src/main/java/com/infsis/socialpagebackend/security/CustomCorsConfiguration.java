5f97bb616509ca809992d8b2b7afe773548f7421
FabDevX <201809660@est.umss.edu>
2025-06-12 08:29:34 +0100

    cors fixed

--- src/main/java/com/infsis/socialpagebackend/security/CustomCorsConfiguration.java
+++ src/main/java/com/infsis/socialpagebackend/security/CustomCorsConfiguration.java
@@ -12,7 +12,7 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:4200", "http://127.0.0.1:8080"));
+        config.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:4200", "http://127.0.0.1:8080", "https://iug.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
